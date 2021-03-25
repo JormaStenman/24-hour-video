02388e985d8ef48b3c8411767da4f519985840e5
Jorma Stenman <jorma.stenman@profitconsulting.fi>
2021-03-25 16:17:13 +0200

    suppress unresolved vars

--- lambda/set-permissions/index.js
+++ lambda/set-permissions/index.js
@@ -4,10 +4,13 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
 exports.handler = (event, context, callback) => {
+    // noinspection JSUnresolvedVariable
     const text = event.Records[0].Sns.Message;
     console.log('message:', text);
     const message = JSON.parse(text);
+    // noinspection JSUnresolvedVariable
     const sourceBucket = message.Records[0].s3.bucket.name;
+    // noinspection JSUnresolvedVariable
     const sourceKey = decodeURIComponent(message.Records[0].s3.object.key.replace(/\+/g, ''));
     console.log('sourceBucket:', sourceBucket);
     console.log('sourceKey:', sourceKey);
